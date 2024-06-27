a23f96f427d898353c6cb9b8adf5fe43f05a134c
prakhar28102005 <141772221+prakhar28102005@users.noreply.github.com>
2024-06-27 22:38:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ button.forEach((e)=>{
     e.addEventListener('click', f);         
 })
 
+function f(buttons){
+    if(buttons.target.innerHTML=='='){
+        let result=execute(textField.value);
+        textField.value=result;
+        string=result;
+        records.push(string);
+    }
+
+    else if(buttons.target.innerHTML=='⏱'){
+        let result=history();
+        textField.value=result;
+        string=result;
+    }
+}
+
 function execute(expression){
     try {
         return eval(expression);

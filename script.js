5adf1950d8dcb3977c36d281a803d2cbf29d9deb
divyansh1010x <b23cm1015@iitj.ac.in>
2024-06-28 00:11:41 +0530

    added keyboard utility

--- script.js
+++ script.js
@@ -18,6 +18,27 @@ function f(buttons) {
     }
 }
 
+document.addEventListener('keydown', g);
+
+function g(event){
+    const arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*'];
+    if(event.key === 'Enter'){
+        let result = execute(textField.value);
+        textField.value = result;
+        let string = result;  
+        records.push(string);
+    }
+    else if(event.key === 'Backspace'){
+        textField.value = textField.value.slice(0, -1);
+    }
+    else if(event.key === 'C' || event.key === 'c' || event.key === 'Delete'){
+        textField.value = '';
+    }
+    else if(arr.includes(event.key)){
+        textField.value += event.key;
+    }
+}
+
 function execute(expression) {
     try {
         return eval(expression);

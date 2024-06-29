a3d6ce512dcdeac5d76816e988885d2f4a1c3f27
divyansh1010x <b23cm1015@iitj.ac.in>
2024-06-29 11:13:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('keydown', g);
 
 function g(event){
     buttonAnimation(event.key);
-    const arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*'];
+    const arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*', '.'];
     if(event.key === 'Enter'){
         let result = execute(textField.value);
         textField.value = result;
